6021a2a8ec61d8ee8e562dd241860e77af71b4c9
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-11 23:59:40 +0500

    d

--- OpenWeatherWithCombine/ViewController.swift
+++ OpenWeatherWithCombine/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import Combine
+
+enum WeatherError: Error {
+    case invalidData
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    private let openWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}"
+    private let keyAPI = "2f30fa8890e957cad6b72824a417b042"
+    
+    private var cancellable: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func newButton(_ sender: Any) {
+        guard let cityName = cityTextField.text else { return }
+        getTemperature(for: cityName)
+    }
+    
+    @IBAction func searchPressedButton(_ sender: Any) {
+        guard let cityName = cityTextField.text else { return }
+        getTemperature(for: cityName)
+    }
+    
+    private func getTemperature(for cityName: String) {
+        guard let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(keyAPI)") else { return }
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: weatherURL)
+            .tryMap { data, response -> Data in
+                guard
+                    let httpResponse = response as? HTTPURLResponse,
+                    httpResponse.statusCode == 200 else {
+                        throw WeatherError.invalidData
+                    }
+                return data
+            }
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: RunLoop.main)
+            .sink(receiveCompletion: { completion in
+                
+            }, receiveValue: { temp in
+                print(temp)
+            })
+    }
 }
 
